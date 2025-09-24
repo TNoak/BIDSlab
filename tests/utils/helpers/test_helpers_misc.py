3ac8a3651ee3384d55dc7921a9d0fc8e652cd6c2
Lukas Behammer <lukas.behammer@uni-a.de>
2025-09-24 14:27:04 +0200

    refactor: refactor tests for set_attr_from_dict

--- tests/utils/helpers/test_helpers_misc.py
+++ tests/utils/helpers/test_helpers_misc.py
@@ -7,39 +7,38 @@
 
 import pytest
 
+import abidskit as abk
 from abidskit.utils.exceptions import FieldNotValidError
-from abidskit.utils.helpers import set_attr_from_dict
 
 
 class SampleClass:
-    def __init__(self, **kwargs):
+    def __init__(self):
         self.attr1 = None
         self.attr2 = None
 
 
-@pytest.fixture
-def create_sample_class():
-    return SampleClass()
-
-
 class TestSetClassAttributes:
-    def test_set_class_attributes_from_dict(self, create_sample_class):
+    def test_set_class_attributes_from_dict(self):
         # Setup
         attr_dict = {"attr1": "value1", "attr2": 42}
+        sample = SampleClass()
 
         # Test
-        set_attr_from_dict(create_sample_class, attr_dict)
+        abk.utils.helpers.set_attr_from_dict(sample, attr_dict)
 
-        assert create_sample_class.attr1 == "value1"
-        assert create_sample_class.attr2 == 42
+        assert sample.attr1 == "value1"
+        assert sample.attr2 == 42
 
-    def test_set_class_attributes_with_invalid_key(self, create_sample_class):
+    def test_set_class_attributes_with_invalid_key(self):
         # Setup
         attr_dict = {"attr1": "value1", "attr3": "value3"}
+        sample = SampleClass()
 
         # Test
-        assert not hasattr(create_sample_class, "attr3")
+        assert not hasattr(sample, "attr3")
         with pytest.raises(FieldNotValidError):
-            set_attr_from_dict(create_sample_class, attr_dict)
+            abk.utils.helpers.set_attr_from_dict(sample, attr_dict)
+
+        assert not hasattr(sample, "attr3")
 
         assert not hasattr(create_sample_class, "attr3")
