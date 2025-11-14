bd3180f3be3f23f5667e99059d70649c619e6cd5
Lukas Behammer <lukas.behammer@uni-a.de>
2025-11-14 15:16:09 +0100

    feat: add channels and reference frame for motion data
    add class MotionChannel, add dataclass ReferenceFrame, add function get_motion_channels, add function get_reference_frames, add representation method for class TrackSys

--- src/abidskit/extensions/motion.py
+++ src/abidskit/extensions/motion.py
@@ -9,27 +9,80 @@ import json
 import os
 import pathlib
 import re
+from dataclasses import dataclass
 from types import SimpleNamespace
 from typing import Any, Iterable, Mapping
 from warnings import warn
 
 from abidskit.common.base import BaseAcquisition, BaseTask
-from abidskit.common.specs_misc import Hardware, Institution
+from abidskit.common.specs_misc import Column, Hardware, Institution
 from abidskit.common.specs_run import Run
 from abidskit.utils.exceptions import TopLevelEntityNotLinkedWarning
 from abidskit.utils.helpers import (
+    add_entity_to_list,
     get_entity_from_file,
     get_tsv_json_files,
+    parse_descriptive_tsv,
+    parse_json_sidecar,
     set_attr_from_dict,
 )
 from abidskit.utils.string_manipulation import to_snakecase
+
+
+@dataclass(slots=True)
+class ReferenceFrame:
+    name: str
+    rotation_order: str | None = None
+    rotation_rule: str | None = None
+    spatial_axes: str | None = None
+    description: str | None = None
+
+    def __repr__(self) -> str:
+        return f"<ReferenceFrame name={self.name}>"
+
+
+class MotionChannel:
+    def __init__(
+        self,
+        name: str,
+        component: str,
+        type: str,  # noqa: A002
+        tracked_point: str,
+        units: str,
+        **kwargs: Any,
+    ):
+        self.name: str = name
+        self.component: str = component
+        self._type: str = type
+        self.tracked_point: str = tracked_point
+        self.units: str = units
+        self.placement: str | None = None
+        self.reference_frame: str | None = None
+        self.description: str | None = None
+        self.sampling_frequency: int | float | None = None
+        self.status: str | None = None
+        self.status_description: str | None = None
+        self.columns: Iterable[Column] | None = None
+
+        set_attr_from_dict(self, kwargs)
+
+    def __repr__(self) -> str:
+        return f"<MotionChannel name={self.name}>"
+
+    @property
+    def type(self) -> str:
+        return self._type
+
+    @type.setter  # noqa: A003
+    def type(self, value: str) -> None:
+        self._type = value
 
 
 class MotionRun(Run):
     def __init__(self, base_path: os.PathLike | str, run_id: str, **kwargs):
         super().__init__(base_path=base_path, run_id=run_id)
 
-        self._channels = None
+        self._channels: Iterable[MotionChannel] | None = None
         self._data: Any = None
 
         set_attr_from_dict(self, kwargs)
@@ -52,6 +105,14 @@ class MotionRun(Run):
     @acquisition.setter
     def acquisition(self, value: "MotionAcquisition") -> None:
         self._acquisition = value
+
+    @property
+    def channels(self) -> Iterable[MotionChannel]:
+        return self._channels
+
+    @channels.setter
+    def channels(self, value: Iterable[MotionChannel]) -> None:
+        self._channels = value
 
     @property
     def data(self):
@@ -95,8 +156,6 @@ class MotionAcquisition(BaseAcquisition):
 
         set_attr_from_dict(self, kwargs)
 
-        # TODO: parse channels in this class
-
     @property
     def tracking_system(self) -> SimpleNamespace | None:
         if self._tracking_system:
@@ -120,6 +179,11 @@ class MotionAcquisition(BaseAcquisition):
     @property
     def runs(self) -> Iterable[MotionRun]:
         if not self._runs:
+            tsv_path, json_path = get_tsv_json_files(
+                self.root,
+                f"*tracksys-{self.tracking_system.tracking_system_id}*_channels",
+            )
+            channels = get_motion_channels(tsv_path=tsv_path, json_path=json_path)
             self._runs = []
             files = self.root.iterdir()
             run_ids = set()
@@ -139,6 +203,7 @@ class MotionAcquisition(BaseAcquisition):
                         run_id=run_id,
                         base_path=self.root,
                         acquisition=self,
+                        channels=channels,
                     )
                 )
 
@@ -149,6 +214,7 @@ class MotionAcquisition(BaseAcquisition):
                         run_id="run-00",
                         base_path=self.root,
                         acquisition=self,
+                        channels=channels,
                     )
                 )
 
@@ -195,7 +261,9 @@ class TrackSys:
         set_attr_from_dict(self, kwargs)
 
     def __repr__(self) -> str:
-        return f"TrackSys(tracking_system_id={self.tracking_system_id}, tracking_system_name={self.tracking_system_name})"
+        return (
+            f"<TrackSys id={self.tracking_system_id}, name={self.tracking_system_name}>"
+        )
 
     @property
     def hardware(self) -> Hardware | None:
@@ -394,3 +462,58 @@ def parse_motion_json_sidecar(sidecar_path: pathlib.Path) -> dict:
             "institution": institution_information,
             "motion": motion_information,
         }
+
+
+def get_reference_frames(reference_frames_data: dict) -> dict[str, ReferenceFrame]:
+    reference_frames_dict = {}
+    for ref_frame_name, ref_frame_values in reference_frames_data["Levels"].items():
+        ref_frame_values_snakecase = {}
+        for key, val in ref_frame_values.items():
+            ref_frame_values_snakecase[to_snakecase(key)] = val
+        reference_frames_dict[ref_frame_name] = ReferenceFrame(
+            name=ref_frame_name, **ref_frame_values_snakecase
+        )
+
+    return reference_frames_dict
+
+
+def get_motion_channels(
+    tsv_path: pathlib.Path | None,
+    json_path: pathlib.Path | None,
+) -> Iterable[MotionChannel]:
+    motion_channels: Iterable[MotionChannel] = []
+    columns = []
+    reference_frames_dict = None
+
+    if json_path:
+        column_data = parse_json_sidecar(json_path)
+
+        reference_frames_data = column_data.pop("reference_frame", None)
+        if reference_frames_data:
+            reference_frames_dict = get_reference_frames(reference_frames_data)
+
+        for column_name, column_values in column_data.items():
+            columns.append(Column(column_name=column_name, **column_values))
+
+    if tsv_path:
+        data = parse_descriptive_tsv(tsv_path)
+        for motion_channel in data:
+            assert isinstance(motion_channel, dict)
+
+            reference_frame_name = motion_channel.pop("reference_frame")
+            if (
+                reference_frames_dict is not None
+                and reference_frame_name in reference_frames_dict
+            ):
+                reference_frame = reference_frames_dict[reference_frame_name]
+            else:
+                reference_frame = reference_frame_name
+            add_entity_to_list(
+                entity_list=motion_channels,
+                entity_class=MotionChannel,
+                columns=columns,
+                reference_frame=reference_frame,
+                **motion_channel,
+            )
+
+    return motion_channels
