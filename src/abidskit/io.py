e0c7044563a283583d877e2c5af83d02fe1b93fe
Lukas Behammer <lukas.behammer@uni-a.de>
2026-09-08 17:43:19 +0200

    docs: document io.py

--- src/abidskit/io.py
+++ src/abidskit/io.py
@@ -1,3 +1,11 @@
+"""I/O functions for BIDS datasets.
+
+This module provides functions to load and write BIDS datasets using the aBIDSkit
+library. It wraps around the :py:class:`~abidskit.common.specs_dataset.Dataset` class
+to provide easier access to loading and saving datasets via simple function calls.
+
+"""
+
 #  Copyright (c) 2025 by Lukas Behammer
 #  University of Augsburg
 #  Department of Computer Science
@@ -12,6 +20,30 @@ from abidskit.common.specs_dataset import Dataset
 
 
 def load_dataset(path: os.PathLike | str, bids_version: str = "1.10.1") -> Dataset:
+    """Load a BIDS dataset as a dataset object from the specified path on disk.
+
+    This function loads a BIDS dataset located at the given file system path and
+    returns it as a :py:class:`~abidskit.common.specs_dataset.Dataset` object. The
+    dataset is validated against the specified BIDS version. This function wraps around
+    the :py:class:`~abidskit.common.specs_dataset.Dataset` class and its
+    :py:meth:`~abidskit.common.specs_dataset.Dataset.load` method
+    from :py:mod:`~abidskit.common.specs_dataset`.
+
+    Parameters
+    ----------
+    path : os.PathLike | str
+        The file system path to the root of the BIDS dataset.
+    bids_version : str, optional
+        The BIDS version to validate against. Default is "1.10.1", the current latest
+        stable version.
+
+    Returns
+    -------
+    Dataset
+        The loaded BIDS dataset as a :py:class:`~abidskit.common.specs_dataset.Dataset`
+        object.
+
+    """
     if not isinstance(path, pathlib.Path):
         path = pathlib.Path(path)
 
