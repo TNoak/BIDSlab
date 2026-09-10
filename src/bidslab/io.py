802258b5f34e4237f69c08643e33a4c284b0c91a
Tobias Noak <tobias.noak@student.uni-augsburg.de>
2026-09-10 15:46:56 +0200

    fix: load_dataset now possible with relative path

--- src/bidslab/io.py
+++ src/bidslab/io.py
@@ -14,7 +14,7 @@ from bidslab.common.specs_dataset import Dataset
 def load_dataset(path: os.PathLike | str, bids_version: str = "1.10.1") -> Dataset:
     if not isinstance(path, pathlib.Path):
         path = pathlib.Path(path)
-
+    path = path.resolve()
     bids_dataset = Dataset(root=path, bids_version=bids_version)
     bids_dataset.load()
 
